cfa8554366d5d0d3e7b49f68f083ac8987dc7d3a
Roberto López <rob.lopez.g@gmail.com>
2018-03-15 18:46:23 +0100

    Add custom issue methods to validator client

--- src/Client/ValidatorClient.php
+++ src/Client/ValidatorClient.php
@@ -5,6 +5,7 @@ namespace Randock\VisaCenterApi\Client;
 use Randock\VisaCenterApi\Exception\OrderNotFoundException;
 use Symfony\Component\HttpFoundation\Request;
 use Randock\Utils\Http\Exception\HttpException;
+use Symfony\Component\HttpFoundation\Response;
 
 class ValidatorClient extends AbstractClient
 {
@@ -27,5 +28,78 @@ class ValidatorClient extends AbstractClient
         }
 
         return $response;
+    }
+
+    /**
+     * @param string $orderId
+     *
+     * @return \stdClass
+     *
+     * @throws OrderNotFoundException
+     */
+    public function getCustomIssue(string $orderId): \stdClass
+    {
+        try{
+            $response = $this->toStdClass($this->request(
+                Request::METHOD_GET,
+                sprintf('/api/issues/%s/custom.json', $orderId)
+            ));
+        }catch (HttpException $exception) {
+            throw new OrderNotFoundException();
+        }
+
+        return $response;
+    }
+
+    /**
+     * @param array $data
+     *
+     * @param string $orderId
+     * @throws \Randock\Utils\Http\Exception\FormErrorsException
+     */
+    public function createCustomIssue(array $data, string $orderId): void
+    {
+        try {
+            $this->request(
+                Request::METHOD_POST,
+                sprintf('/api/issues/%s/custom.json', $orderId),
+                [
+                    $data,
+                ]
+            );
+
+        } catch (HttpException $exception) {
+            if (Response::HTTP_BAD_REQUEST === $exception->getStatusCode()) {
+                $this->throwFormErrorsException($exception);
+            }
+
+            throw $exception;
+        }
+    }
+
+    /**
+     * @param array $data
+     *
+     * @param string $orderId
+     * @throws \Randock\Utils\Http\Exception\FormErrorsException
+     */
+    public function updateCustomIssue(array $data, string $orderId): void
+    {
+        try {
+            $this->request(
+                Request::METHOD_PATCH,
+                sprintf('/api/issues/%s/custom.json', $orderId),
+                [
+                    $data,
+                ]
+            );
+
+        } catch (HttpException $exception) {
+            if (Response::HTTP_BAD_REQUEST === $exception->getStatusCode()) {
+                $this->throwFormErrorsException($exception);
+            }
+
+            throw $exception;
+        }
     }
 }
